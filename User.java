c2efcef6ee8df641d28b756ca962b99a811649e7
OmerM01 <omerwork00@gmail.com>
2025-01-06 14:11:34 +0200

    HW_01

--- User.java
+++ User.java
@@ -43,20 +43,26 @@
 
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
-        //// Replace the following statement with your code
+        for(int i = 0; i < this.fCount; i++){
+            if(this.follows[i].equals(name)) return true;
+        }
         return false;
     }
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        //// Replace the following statement with your code
-        return false;
+        if(this.fCount >= maxfCount) return false;
+        if(follows(name)) return false;
+        this.follows[fCount] = name;
+        this.fCount++;
+        return true;
     }
 
     /** Removes the given name from the follows list of this user. If successful, returns true.
      *  If the name is not in the list, does nothing and returns false. */
     public boolean removeFollowee(String name) {
-        //// Replace the following statement with your code
+        if(!follows(name)) return false;
+        
         return false;
     }
 
