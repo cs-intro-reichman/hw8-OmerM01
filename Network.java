790875888c95a12b0d7232c26cb984119cd80a2a
OmerM01 <omerwork00@gmail.com>
2025-01-07 18:32:46 +0200

    HW_14

--- Network.java
+++ Network.java
@@ -122,8 +122,8 @@ public class Network {
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
         String result= "Network:\n";
-       for (int i = 0; i < this.userCount; i++) {
-           result += this.users[i].toString() + "\n";
+       for (int i = 0; i < userCount; i++) {
+           result += users[i].toString() + "\n";
        }
        return result;
     }
